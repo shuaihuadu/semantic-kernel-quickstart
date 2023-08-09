9eaadc60f39a1ec2e23aee99b055f095d1072e5c
ShuaiHua Du <shuaihua.du@outlook.com>
2023-08-09 18:56:41 +0800

    Add Qdrant samples.

---
+++ QuickStart.Connectors.Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using IdeaTech.QuickStart.Configurations;
+global using IdeaTech.QuickStart.Tests;
+global using Microsoft.SemanticKernel;
+global using Microsoft.SemanticKernel.Connectors.Memory.Qdrant;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
