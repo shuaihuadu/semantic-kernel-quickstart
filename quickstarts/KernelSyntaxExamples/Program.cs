f960d2f9fbca9152ade82e4c8fc684e04107168f
ShuaiHua Du <shuaihua.du@outlook.com>
2024-01-08 00:17:36 +0800

    Add Example01_MethodFunctions.cs

---
+++ quickstarts/KernelSyntaxExamples/Program.cs
@@ -0,0 +1,27 @@
+﻿using KernelSyntaxExamples;
+
+const string filter = "";
+
+using CancellationTokenSource cts = new();
+CancellationToken cancellationToken = cts.ConsoleCancellationToken();
+await RunExamplesAsync(filter, cancellationToken);
+
+static async Task RunExamplesAsync(string? filter, CancellationToken cancellationToken)
+{
+    await Example01_MethodFunctions.RunAsync();
+}
+
+public static class Extensions
+{
+    public static CancellationToken ConsoleCancellationToken(this CancellationTokenSource tokenSource)
+    {
+        Console.CancelKeyPress += (s, e) =>
+        {
+            Console.WriteLine("Canceling...");
+            tokenSource.Cancel();
+            e.Cancel = true;
+        };
+
+        return tokenSource.Token;
+    }
+}
