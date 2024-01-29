7d1d7e1ab2d51ef582a18a02983424e1785851dd
ShuaiHua Du <shuaihua.du@outlook.com>
2024-01-29 23:21:47 +0800

    Add examples.

---
+++ quickstarts/KernelSyntaxExamples/Getting_Started/Step7_Observability.cs
@@ -0,0 +1,137 @@
+﻿namespace KernelSyntaxExamples;
+
+public class Step7_Observability : BaseTest
+{
+
+    [Fact]
+    public async Task ObservabilityWithFiltersAsync()
+    {
+        IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+
+        kernelBuilder.AddAzureOpenAIChatCompletion(
+            deploymentName: TestConfiguration.AzureOpenAI.DeploymentName,
+            endpoint: TestConfiguration.AzureOpenAI.Endpoint,
+            apiKey: TestConfiguration.AzureOpenAI.ApiKey);
+
+        kernelBuilder.Plugins.AddFromType<TimeInformation>();
+
+        kernelBuilder.Services.AddSingleton(this.Output);
+        kernelBuilder.Services.AddSingleton<IFunctionFilter, MyFunctionFilter>();
+
+        Kernel kernel = kernelBuilder.Build();
+
+        kernel.PromptFilters.Add(new MyPromptFilter(this.Output));
+
+        OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
+
+        FunctionResult result = await kernel.InvokePromptAsync("How many days until Christmas? Explain your thinking.", new KernelArguments(settings));
+
+        WriteLine(result.ToString());
+    }
+
+    [Fact]
+    [Obsolete("Events are deprecated in favor of filters.")]
+    public async Task ObservabilityWithHooksAsync()
+    {
+        IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+
+        kernelBuilder.AddAzureOpenAIChatCompletion(
+            deploymentName: TestConfiguration.AzureOpenAI.DeploymentName,
+            endpoint: TestConfiguration.AzureOpenAI.Endpoint,
+            apiKey: TestConfiguration.AzureOpenAI.ApiKey);
+
+        kernelBuilder.Plugins.AddFromType<TimeInformation>();
+
+        Kernel kernel = kernelBuilder.Build();
+
+        void MyInvokingHandler(object? sender, FunctionInvokingEventArgs e)
+        {
+            WriteLine($"Invoking {e.Function.Name}");
+        }
+
+        void MyRenderingHandler(object? sender, PromptRenderingEventArgs e)
+        {
+            WriteLine($"Rendering prompt for {e.Function.Name}");
+        }
+
+        void MyRenderedHandler(object? sender, PromptRenderedEventArgs e)
+        {
+            WriteLine($"Prompt sent to model: {e.RenderedPrompt}");
+        }
+
+        void MyInvokedHandler(object? sender, FunctionInvokedEventArgs e)
+        {
+            if (e.Result.Metadata is not null && e.Result.Metadata.ContainsKey("Usage"))
+            {
+                WriteLine($"Token usage: {e.Result.Metadata?["Usage"]?.AsJson()}");
+            }
+        }
+
+        kernel.FunctionInvoking += MyInvokingHandler;
+        kernel.PromptRendering += MyRenderingHandler;
+        kernel.PromptRendered += MyRenderedHandler;
+        kernel.FunctionInvoked += MyInvokedHandler;
+
+        OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
+
+        FunctionResult result = await kernel.InvokePromptAsync("How many days until Christmas? Explain your thinking.", new KernelArguments(settings));
+
+        WriteLine(result.ToString());
+    }
+
+    private sealed class TimeInformation
+    {
+        [KernelFunction]
+        [Description("Retrieves the current time in UTC.")]
+        public string GetCurrentUtcTime() => DateTime.UtcNow.ToString("R");
+    }
+
+    private sealed class MyFunctionFilter : IFunctionFilter
+    {
+        private readonly ITestOutputHelper _output;
+
+        public MyFunctionFilter(ITestOutputHelper output)
+        {
+            this._output = output;
+        }
+
+        public void OnFunctionInvoked(FunctionInvokedContext context)
+        {
+            IReadOnlyDictionary<string, object?>? metadata = context.Result.Metadata;
+
+            if (metadata is not null && metadata.ContainsKey("Usage"))
+            {
+                this._output.WriteLine($"Token usage: {metadata["Usage"]?.AsJson()}");
+            }
+        }
+
+        public void OnFunctionInvoking(FunctionInvokingContext context)
+        {
+            this._output.WriteLine($"Invoking {context.Function.Name}");
+        }
+    }
+
+    private sealed class MyPromptFilter : IPromptFilter
+    {
+        private readonly ITestOutputHelper _output;
+
+        public MyPromptFilter(ITestOutputHelper output)
+        {
+            this._output = output;
+        }
+
+        public void OnPromptRendered(PromptRenderedContext context)
+        {
+            this._output.WriteLine($"Prompt sent to model: {context.RenderedPrompt}");
+        }
+
+        public void OnPromptRendering(PromptRenderingContext context)
+        {
+            this._output.WriteLine($"Rendering prompt for {context.Function.Name}");
+        }
+    }
+
+    public Step7_Observability(ITestOutputHelper output) : base(output)
+    {
+    }
+}
---
+++ quickstarts/KernelSyntaxExamples/Getting_Started/Step8_Pipelining.cs
@@ -0,0 +1,152 @@
+﻿
+namespace KernelSyntaxExamples;
+
+public class Step8_Pipelining : BaseTest
+{
+
+    [Fact]
+    public async Task RunAsync()
+    {
+        IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+
+        kernelBuilder.AddAzureOpenAIChatCompletion(
+            deploymentName: TestConfiguration.AzureOpenAI.DeploymentName,
+            endpoint: TestConfiguration.AzureOpenAI.Endpoint,
+            apiKey: TestConfiguration.AzureOpenAI.ApiKey);
+
+        kernelBuilder.Services.AddLogging(c => c.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+        Kernel kernel = kernelBuilder.Build();
+
+        WriteLine("================ PIPELINE ================");
+        {
+            // Create a pipeline of functions that will parse a string into an int, multiply it by a double, truncate it to an int, and then humanize it.
+            KernelFunction parseInt32 = KernelFunctionFactory.CreateFromMethod((string s) => double.Parse(s, CultureInfo.InvariantCulture), "parseInt32");
+            KernelFunction multiplyByN = KernelFunctionFactory.CreateFromMethod((double i, double n) => i * n, "multiplyByN");
+            KernelFunction truncate = KernelFunctionFactory.CreateFromMethod((double d) => (int)d, "truncate");
+            KernelFunction humanize = KernelFunctionFactory.CreateFromPrompt(new PromptTemplateConfig
+            {
+                Template = "Spell out this number in English: {{$number}}",
+                InputVariables = new() { new() { Name = "number" } }
+            });
+
+            KernelFunction pipeline = KernelFunctionCombinators.Pipe(new[] { parseInt32, multiplyByN, truncate, humanize }, "pipeline");
+
+            KernelArguments arguments = new()
+            {
+                ["s"] = "10",
+                ["n"] = (double)78.9
+            };
+
+            FunctionResult functionResult = await pipeline.InvokeAsync(kernel, arguments);
+
+            WriteLine(functionResult.ToString());
+        }
+
+        WriteLine("================ GRAPH ================");
+        {
+            KernelFunction rand = KernelFunctionFactory.CreateFromMethod(() => Random.Shared.Next(), "GetRandomInt32");
+            KernelFunction mult = KernelFunctionFactory.CreateFromMethod((int i, int j) => i * j, "Multiply");
+
+            KernelFunction graph = KernelFunctionCombinators.Pipe(new[]
+            {
+                (rand,"i"),
+                (rand,"j"),
+                (mult,"")
+            }, "graph");
+
+            FunctionResult functionResult = await graph.InvokeAsync(kernel);
+
+            WriteLine(functionResult.ToString());
+        }
+    }
+
+    public Step8_Pipelining(ITestOutputHelper output) : base(output)
+    {
+    }
+}
+
+public static class KernelFunctionCombinators
+{
+    public static Task<FunctionResult> InvokePipelineAsync(
+        IEnumerable<KernelFunction> functions,
+        Kernel kernel,
+        KernelArguments arguments,
+        CancellationToken cancellationToken)
+        => Pipe(functions).InvokeAsync(kernel, arguments, cancellationToken);
+
+    public static Task<FunctionResult> InvokePipelineAsync(
+        IEnumerable<(KernelFunction Function, string OutputVariable)> functions,
+        Kernel kernel,
+        KernelArguments arguments,
+        CancellationToken cancellationToken)
+        => Pipe(functions).InvokeAsync(kernel, arguments, cancellationToken);
+
+
+    public static KernelFunction Pipe(
+        IEnumerable<KernelFunction> functions,
+        string? functionName = null,
+        string? description = null)
+    {
+        ArgumentNullException.ThrowIfNull(functions, nameof(functions));
+
+        KernelFunction[] kernelFunctions = functions.ToArray();
+
+        Array.ForEach(kernelFunctions, f => ArgumentNullException.ThrowIfNull(f));
+
+        (KernelFunction Function, string OutputVariable)[] funcsAndVars = new (KernelFunction Function, string OutputVariable)[kernelFunctions.Length];
+
+        for (int i = 0; i < kernelFunctions.Length; i++)
+        {
+            string p = string.Empty;
+
+            if (i < kernelFunctions.Length - 1)
+            {
+                IReadOnlyList<KernelParameterMetadata> kernelParameters = kernelFunctions[i + 1].Metadata.Parameters;
+
+                if (kernelParameters.Count > 0)
+                {
+                    p = kernelParameters[0].Name;
+                }
+            }
+
+            funcsAndVars[i] = (kernelFunctions[i], p);
+        }
+
+        return Pipe(funcsAndVars, functionName, description);
+    }
+
+    public static KernelFunction Pipe(
+        IEnumerable<(KernelFunction Function, string OutputVariable)> functions,
+        string? functionName = null,
+        string? description = null)
+    {
+        ArgumentNullException.ThrowIfNull(functions);
+
+        (KernelFunction Function, string OutputVariable)[] arr = functions.ToArray();
+
+        Array.ForEach(arr, f =>
+        {
+            ArgumentNullException.ThrowIfNull(f.Function);
+            ArgumentNullException.ThrowIfNull(f.OutputVariable);
+        });
+
+        return KernelFunctionFactory.CreateFromMethod(async (Kernel kernel, KernelArguments arguments) =>
+        {
+            FunctionResult? result = null;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result = await arr[i].Function.InvokeAsync(kernel, arguments).ConfigureAwait(false);
+
+                if (i < arr.Length - 1)
+                {
+                    arguments[arr[i].OutputVariable] = result.GetValue<object>();
+                }
+            }
+
+            return result;
+
+        }, functionName, description);
+    }
+}
