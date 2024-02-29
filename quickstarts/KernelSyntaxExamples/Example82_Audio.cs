a96fcdd92f8940101c088982be8ffc7de8315bc4
ShuaiHua Du <shuaihua.du@outlook.com>
2024-02-29 20:31:03 +0800

    Add examples.

--- quickstarts/KernelSyntaxExamples/Example82_Audio.cs
+++ quickstarts/KernelSyntaxExamples/Example82_Audio.cs
@@ -18,11 +18,15 @@ public sealed class Example82_Audio : BaseTest
 
         ITextToAudioService textToAudioService = kernel.GetRequiredService<ITextToAudioService>();
 
-        string sampleText = "Hello, my name is John. I am a software engineer. I am working on a project to convert text to audio";
+        string sampleText = "公共安全行业标准《生物样品血液、尿液中乙醇、甲醇、正丙醇、乙醛、丙酮、异丙醇和正丁醇的顶空—气相色谱检验方法》等标准发布实施以来，为机动车驾驶人血液酒精含量检测提供了技术依据。\r\n\r\n全国刑事技术标准化技术委员会在进一步丰富优化该项行业标准检测分析方法、细化完善有关技术要求的基础上，组织制定了《血液、尿液中乙醇、甲醇、正丙醇、丙酮、异丙醇和正丁醇检验》（GB/T42430-2023）国家标准。";
 
-        OpenAITextToAudioExecutionSettings settings = new("alloy")
+        //Experiment with different voices (alloy, echo, fable, onyx, nova, and shimmer) 
+
+        string voice = "nova";
+
+        OpenAITextToAudioExecutionSettings settings = new(voice)
         {
-            Voice = "alloy",
+            Voice = voice,
             ResponseFormat = "mp3",
             Speed = 1.0f
         };
