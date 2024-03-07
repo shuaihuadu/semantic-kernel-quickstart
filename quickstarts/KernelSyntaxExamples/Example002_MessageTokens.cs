1e8299c8428781a666151dd774d2353d48cc5a84
ShuaiHua Du <shuaihua.du@outlook.com>
2024-03-08 01:30:51 +0800

    Add examples.

--- quickstarts/KernelSyntaxExamples/Example002_MessageTokens.cs
+++ quickstarts/KernelSyntaxExamples/Example002_MessageTokens.cs
@@ -1,5 +1,4 @@
-﻿
-namespace KernelSyntaxExamples;
+﻿namespace KernelSyntaxExamples;
 
 public class Example002_ActualTokenCalculate(ITestOutputHelper output) : BaseTest(output)
 {
@@ -9,9 +8,9 @@ public class Example002_ActualTokenCalculate(ITestOutputHelper output) : BaseTes
         List<Dictionary<string, string>> exampleMessages =
         [
             new() {
-                {"role", "user"},
-                {"content", "请帮我回答一下什么是OpenAI"}
-            }
+            {"role", "user"},
+            {"content", "What is OpenAI?"}
+        }
         ];
 
         int count = this.CalculateTokensCountForMessage(exampleMessages);
@@ -27,10 +26,11 @@ public class Example002_ActualTokenCalculate(ITestOutputHelper output) : BaseTes
         GptEncoding encoding = GptEncoding.GetEncoding("cl100k_base");
 
         //if (model == "gpt-3.5-turbo-0301")
-        //tokensPerMessage = 4;
-        //tokensPerName = -1;
+        //tokensPerMessage = 4; // every message follows <|start|>{role/name}\n{content}<|end|>\n
+        //tokensPerName = -1; // if there's a name, the role is omitted
 
         int numTokens = 0;
+
         foreach (var message in messages)
         {
             numTokens += tokensPerMessage;
@@ -48,7 +48,7 @@ public class Example002_ActualTokenCalculate(ITestOutputHelper output) : BaseTes
             }
         }
 
-        numTokens += 3;
+        numTokens += 3; //every reply is primed with <|start|>assistant<|message|>
 
         return numTokens;
     }
