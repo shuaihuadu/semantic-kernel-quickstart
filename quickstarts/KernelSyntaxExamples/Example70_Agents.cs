620eec675ce7c70548672801ddf96f47d5e92b05
ShuaiHua Du <shuaihua.du@outlook.com>
2024-03-15 23:55:12 +0800

    Add examples.

--- quickstarts/KernelSyntaxExamples/Example70_Agents.cs
+++ quickstarts/KernelSyntaxExamples/Example70_Agents.cs
@@ -1,14 +1,71 @@
 ﻿namespace KernelSyntaxExamples;
 
-public class Example70_Agents : BaseTest
+public class Example70_Agents(ITestOutputHelper output) : BaseTest(output)
 {
-    [Fact(Skip = "When Agent Support Azure OpenAI")]
+    [Fact]
     public Task RunAsync()
     {
         return Task.CompletedTask;
     }
 
-    public Example70_Agents(ITestOutputHelper output) : base(output)
+    //[Fact]
+    //public Task RunWithMethodFunctionsAsync()
+    //{
+    //    WriteLine("======== Run:WithMethodFunctions ========");
+
+    //    KernelPlugin plugin = KernelPluginFactory.CreateFromType<MenuPlugin>();
+    //}
+
+    [Fact(Skip = "Microsoft.SemanticKernel.HttpOperationException : Incorrect API key provided: You can find your API key at https://platform.openai.com/account/api-keys.")]
+    public Task RunWithPromptFunctionsAsync()
     {
+        WriteLine("======== WithPromptFunctions ========");
+
+        KernelFunction function = KernelFunctionFactory.CreateFromPrompt(
+             "Correct any misspelling or gramatical errors provided in input: {{$input}}",
+            functionName: "SpellChecker",
+            description: "Correct the spelling for the user input.");
+
+        KernelPlugin plugin = KernelPluginFactory.CreateFromFunctions("spelling", "Spelling functions", [function]);
+
+        return ChatAsync(
+            "Agents.ToolAgent.yaml",
+            plugin,
+            arguments: null,
+            "Hello",
+            "Is this spelled correctly: exercize",
+            "What is the special soup?",
+            "Thank you!");
+    }
+
+    private async Task ChatAsync(string resourcePath, KernelPlugin? plugin = null, KernelArguments? arguments = null, params string[] messages)
+    {
+        string definition = EmbeddedResource.Read(resourcePath);
+
+        IAgent agent = await new AgentBuilder()
+            .WithAzureOpenAIChatCompletion(TestConfiguration.AzureOpenAI.Endpoint, TestConfiguration.AzureOpenAI.ChatModelId, TestConfiguration.AzureOpenAI.ApiKey)
+            .FromTemplate(definition)
+            .WithPlugin(plugin)
+            .BuildAsync();
+
+        IAgentThread thread = await agent.NewThreadAsync();
+
+        try
+        {
+            WriteLine($"[{agent.Id}]");
+
+            foreach (var responses in messages.Select(m => thread.InvokeAsync(agent, m, arguments)))
+            {
+                await foreach (var message in responses)
+                {
+                    WriteLine($"[{message.Id}]");
+                    WriteLine($"# {message.Role}: {message.Content}");
+                }
+            }
+        }
+        finally
+        {
+            await Task.WhenAll(thread?.DeleteAsync() ?? Task.CompletedTask, agent.DeleteAsync());
+        }
     }
 }
---
+++ quickstarts/KernelSyntaxExamples/Plugins/MenuPlugin.cs
@@ -0,0 +1,26 @@
+﻿namespace KernelSyntaxExamples.Plugins;
+
+internal sealed class MenuPlugin
+{
+    [KernelFunction, Description("Providers a list of specials from the menu.")]
+    public string SendEmail(
+        [Description("The body of the email message to send.")] string input,
+        [Description("The email address to send email to.")] string email_address)
+        => $"Send email to: {email_address}. Body: {input}";
+
+    [KernelFunction, Description("Given a name, find email address")]
+    public string GetEmailAddress(
+        [Description("The name of the person whose email address needs to be found.")] string input,
+        ILogger? logger = null)
+    {
+        logger?.LogTrace("Returning hard coded email for {0}", input);
+
+        return input switch
+        {
+            "Zhangsan" => "zhangsan@email.com",
+            "Lisi" => "lisi@email.com",
+            "Wangwu" => "wangwu@email.com",
+            _ => "test@email.com"
+        };
+    }
+}
