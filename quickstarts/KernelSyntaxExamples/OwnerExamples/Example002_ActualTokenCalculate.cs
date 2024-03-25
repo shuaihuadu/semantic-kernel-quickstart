f73af8d8915478be28cded3a84a82479aac36a99
ShuaiHua Du <shuaihua.du@outlook.com>
2024-03-26 01:07:04 +0800

    Add examples.

---
+++ quickstarts/KernelSyntaxExamples/OwnerExamples/Example002_ActualTokenCalculate.cs
@@ -0,0 +1,104 @@
+﻿namespace KernelSyntaxExamples.OwnerExamples;
+
+public class Example002_ActualTokenCalculate(ITestOutputHelper output) : BaseTest(output)
+{
+
+    [Fact]
+    public async Task RunAsync()
+    {
+        string content = "How are you";
+
+        List<Dictionary<string, string>> exampleMessages =
+        [
+            //new() {
+            //    {"role", "system"},
+            //    {"content", "You are a helpful assistant."}
+            //},
+            new() {
+                {"role", "user"},
+                {"content", content}
+            },
+            //new() {
+            //    {"role", "assistant"},
+            //    {"content", "Hello there, how may I assist you today?"}
+            //}
+        ];
+
+        int count = CalculateTokensCountForMessage(exampleMessages);
+
+        WriteLine(count);
+
+        Kernel kernel = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(
+            deploymentName: TestConfiguration.AzureOpenAI.DeploymentName,
+            endpoint: TestConfiguration.AzureOpenAI.Endpoint,
+            apiKey: TestConfiguration.AzureOpenAI.ApiKey)
+            .Build();
+
+        kernel.PromptFilters.Add(new PromptFilter());
+        kernel.FunctionFilters.Add(new FunctionFilter());
+
+        FunctionResult functionResult = await kernel.InvokePromptAsync(content);
+
+        WriteLine(functionResult.Metadata["Usage"].AsJson());
+
+
+        ChatHistory history = [];
+
+        history.AddUserMessage(content);
+
+        IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+        ChatMessageContent reply = await chatCompletionService.GetChatMessageContentAsync(history);
+
+        WriteLine(reply.Metadata["Usage"].AsJson());
+    }
+
+
+    public int CalculateTokensCountForMessage(List<Dictionary<string, string>> messages)
+    {
+        int tokensPerMessage = 3;
+
+        GptEncoding encoding = GptEncoding.GetEncoding("cl100k_base");
+        //GptEncoding encoding = GptEncoding.GetEncodingForModel("gpt-4");
+
+        int numTokens = 0;
+
+        foreach (var message in messages)
+        {
+            numTokens += tokensPerMessage;
+
+            foreach (var item in message)
+            {
+                List<int> tokens = encoding.Encode(item.Value);
+
+                numTokens += tokens.Count;
+            }
+        }
+
+        numTokens += 3; //every reply is primed with <|start|>assistant<|message|>
+
+        return numTokens;
+    }
+}
+
+public sealed class PromptFilter : IPromptFilter
+{
+    public void OnPromptRendered(PromptRenderedContext context)
+    {
+    }
+
+    public void OnPromptRendering(PromptRenderingContext context)
+    {
+    }
+}
+
+public sealed class FunctionFilter : IFunctionFilter
+{
+    public void OnFunctionInvoked(FunctionInvokedContext context)
+    {
+    }
+
+    public void OnFunctionInvoking(FunctionInvokingContext context)
+    {
+    }
+}
---
+++ quickstarts/KernelSyntaxExamples/OwnerExamples/Example006_TokenCalculator.cs
@@ -0,0 +1,122 @@
+﻿namespace KernelSyntaxExamples.OwnerExamples;
+
+public class Example006_TokenCalculator(ITestOutputHelper output) : BaseTest(output)
+{
+    [Fact]
+    public void Run()
+    {
+        string result = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "OwnerExamples", "result.txt"));
+
+        //List<Dictionary<string, string>> exampleMessages =
+        //[
+        //    new() {
+        //        {"role", "user"},
+        //        {"content", result}
+        //    }
+        //];
+
+        var exampleMessages = new List<Dictionary<string, string>>
+        {
+            new()
+            {
+                {"role", "system"},
+                {"content", "You are a helpful, pattern-following assistant that translates corporate jargon into plain English."},
+            },
+            new()
+            {
+                {"role", "system"},
+                {"name", "example_user"},
+                {"content", "New synergies will help drive top-line growth."},
+            },
+            new()
+            {
+                {"role", "system"},
+                {"name", "example_assistant"},
+                {"content", "Things working well together will increase revenue."},
+            },
+            new()
+            {
+                {"role", "system"},
+                {"name", "example_user"},
+                {"content", "Let's circle back when we have more bandwidth to touch base on opportunities for increased leverage."},
+            },
+            new()
+            {
+                {"role", "system"},
+                {"name", "example_assistant"},
+                {"content", "Let's talk later when we're less busy about how to do better."},
+            },
+            new()
+            {
+                {"role", "user"},
+                {"content", "This late pivot means we don't have time to boil the ocean for the client deliverable."},
+            },
+        };
+
+        WriteLine(NumTokensFromMessages(exampleMessages, "gpt-3.5-turbo-0613"));
+        WriteLine(NumTokensFromMessages(exampleMessages, "gpt-4-0613"));
+    }
+
+    public int NumTokensFromMessages(List<Dictionary<string, string>> messages, string model)
+    {
+        int tokensPerMessage = 3;
+        int tokensPerName = 1;
+
+        GptEncoding encoding = GptEncoding.GetEncoding("cl100k_base");
+
+        HashSet<string> modelSet =
+        [
+            "gpt-3.5-turbo-0613",
+            "gpt-3.5-turbo-16k-0613",
+            "gpt-4-0314",
+            "gpt-4-32k-0314",
+            "gpt-4-0613",
+            "gpt-4-32k-0613",
+        ];
+
+        if (modelSet.Contains(model))
+        {
+            tokensPerMessage = 3;
+            tokensPerName = 1;
+        }
+
+        if (model == "gpt-3.5-turbo-0301")
+        {
+            tokensPerMessage = 4;
+            tokensPerName = -1;
+        }
+
+        if (model.Contains("gpt-3.5-turbo"))
+        {
+            WriteLine("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.");
+        }
+        else if (model.Contains("gpt-4"))
+        {
+            WriteLine("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.");
+        }
+        else
+        {
+            throw new NotImplementedException($"num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.");
+        }
+
+        int numTokens = 0;
+
+        foreach (var message in messages)
+        {
+            numTokens += tokensPerMessage;
+
+            foreach (var item in message)
+            {
+                numTokens += encoding.Encode(item.Value).Count;
+
+                if (item.Key == "name")
+                {
+                    numTokens += tokensPerName;
+                }
+            }
+        }
+
+        numTokens += 3;
+        return numTokens;
+    }
+}
