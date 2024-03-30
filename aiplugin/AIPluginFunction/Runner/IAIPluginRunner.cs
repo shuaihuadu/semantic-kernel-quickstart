e4cde45299e8de0ad77ada18c21a3d5701d172c4
ShuaiHua Du <shuaihua.du@outlook.com>
2024-03-31 01:17:26 +0800

    Add examples.

--- aiplugin/AIPluginFunction/Runner/IAIPluginRunner.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Azure.Functions.Worker.Http;
-using System.Threading.Tasks;
-
-namespace AIPluginFunction.Runner;
-
-public interface IAIPluginRunner
-{
-    Task<HttpResponseData> RunAIPluginOperationAsync(HttpRequestData request, string operationId);
-}
