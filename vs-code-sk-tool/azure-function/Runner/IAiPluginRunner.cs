5dbfa95594dfc313ab8dc0061c314c82605a6e6f
ShuaiHua Du <shuaihua.du@outlook.com>
2024-04-03 02:26:40 +0800

    Add examples.

--- vs-code-sk-tool/azure-function/Runner/IAiPluginRunner.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Azure.Functions.Worker.Http;
-
-namespace AiPlugin.Runner;
-
-public interface IAiPluginRunner
-{
-    Task<HttpResponseData> RunAIPluginOperationAsync(HttpRequestData request, string operationId);
-}
