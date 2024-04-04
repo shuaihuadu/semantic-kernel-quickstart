e20f8a8532cf6b8f969a86f7755e7c609384d0ca
ShuaiHua Du <shuaihua.du@outlook.com>
2024-04-04 22:27:23 +0800

    Add examples.

--- ai-plugin-generator/KernelFunction.AzureFunction.SourceGenerator/KernelFunctionGenerator.cs
+++ ai-plugin-generator/KernelFunction.AzureFunction.SourceGenerator/KernelFunctionGenerator.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace KernelFunction.WebApi.SourceGenerator
 {
