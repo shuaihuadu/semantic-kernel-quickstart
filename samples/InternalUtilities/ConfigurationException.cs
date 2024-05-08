f0e3e519ff55dd7db908fcc59e1120f69d602435
ShuaiHua Du <shuaihua.du@outlook.com>
2024-05-08 23:22:41 +0800

    Add examples.

---
+++ samples/InternalUtilities/ConfigurationException.cs
@@ -0,0 +1,14 @@
+﻿public class ConfigurationException : Exception
+{
+    public ConfigurationException()
+    {
+    }
+
+    public ConfigurationException(string message) : base(message)
+    {
+    }
+
+    public ConfigurationException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
