c839885346ce98b77658bd0cc86e2e65a18ec836
ShuaiHua Du <shuaihua.du@outlook.com>
2024-05-25 01:14:34 +0800

    Add examples.

---
+++ quickstarts/Concepts/Memory/HuggingFace_EmbeddingGeneration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SemanticKernel.Embeddings;
+
+namespace Memory;
+
+public class HuggingFace_EmbeddingGeneration(ITestOutputHelper output) : BaseTest(output)
+{
+
+    [RetryFact(typeof(HttpOperationException), Skip = "TODO Hugging Face ")]
+    public async Task RunInferenceApiEmbeddingAsync()
+    {
+        this.WriteLine("\n======= Hugging Face Inference API - Embedding Example ========\n");
+
+        Kernel kernel = Kernel.CreateBuilder()
+            .AddHuggingFaceTextEmbeddingGeneration(
+                model: TestConfiguration.HuggingFace.EmbeddingModelId,
+                apiKey: TestConfiguration.HuggingFace.ApiKey)
+            .Build();
+
+        ITextEmbeddingGenerationService embeddingGenerator = kernel.GetRequiredService<ITextEmbeddingGenerationService>();
+
+        IList<ReadOnlyMemory<float>> embeddings = await embeddingGenerator.GenerateEmbeddingsAsync(["John: Hello, how are you?\nRoger: Hey, I'm Roger!"]);
+
+        WriteLine($"Generated {embeddings.Count} embeddings for the provided text");
+    }
+}
--- quickstarts/KernelSyntaxExamples/Example20_HuggingFace.cs
+++ quickstarts/KernelSyntaxExamples/Example20_HuggingFace.cs
@@ -28,24 +28,6 @@ public class Example20_HuggingFace(ITestOutputHelper output) : BaseTest(output)
     }
 
     [RetryFact(typeof(HttpOperationException), Skip = "TODO Hugging Face ")]
-    public async Task RunInferenceApiEmbeddingAsync()
-    {
-        this.WriteLine("\n======= Hugging Face Inference API - Embedding Example ========\n");
-
-        Kernel kernel = Kernel.CreateBuilder()
-            .AddHuggingFaceTextEmbeddingGeneration(
-                model: TestConfiguration.HuggingFace.EmbeddingModelId,
-                apiKey: TestConfiguration.HuggingFace.ApiKey)
-            .Build();
-
-        ITextEmbeddingGenerationService embeddingGenerator = kernel.GetRequiredService<ITextEmbeddingGenerationService>();
-
-        IList<ReadOnlyMemory<float>> embeddings = await embeddingGenerator.GenerateEmbeddingsAsync(["John: Hello, how are you?\nRoger: Hey, I'm Roger!"]);
-
-        WriteLine($"Generated {embeddings.Count} embeddings for the provided text");
-    }
-
-    [RetryFact(typeof(HttpOperationException), Skip = "TODO Hugging Face ")]
     public async Task RunStreamingExampleAsync()
     {
         WriteLine("\n======== HuggingFace zephyr-7b-beta streaming example ========\n");
