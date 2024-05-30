b9af7416ef71fbe8af1a4ce67f66439e97e752cc
ShuaiHua Du <shuaihua.du@outlook.com>
2024-05-30 16:52:38 +0800

    Add examples.

---
+++ quickstarts/Concepts/PromptTemplates/MultiplePromptTemplates.cs
@@ -0,0 +1,45 @@
+﻿namespace PromptTemplates;
+
+public class MultiplePromptTemplates(ITestOutputHelper output) : BaseTest(output)
+{
+    [RetryTheory(typeof(HttpOperationException))]
+    [InlineData("semantic-kernel", "Hello AI, my name is {{$name}}. What is the origin of my name?", "Paz")]
+    [InlineData("handlebars", "Hello AI, my name if {{name}}. What is the origin of my name?", "Mira")]
+    [InlineData("liquid", "Hello AI, my name is {{name}}. What is the origin of my name?", "Aoibhinn")]
+    public Task InvokeDifferentPromptTypes(string templateFormat, string prompt, string name)
+    {
+        Console.WriteLine($"======== {nameof(MultiplePromptTemplates)} ========");
+
+        Kernel kernel = KernelHelper.CreateKernelWithAzureOpenAIChatCompletion();
+
+        AggregatorPromptTemplateFactory promptTemplateFactory = new(
+            new KernelPromptTemplateFactory(),
+            new HandlebarsPromptTemplateFactory(),
+            new LiquidPromptTemplateFactory());
+
+        return RunPromptAsync(kernel, prompt, name, templateFormat, promptTemplateFactory);
+    }
+
+    private async Task RunPromptAsync(Kernel kernel, string prompt, string name, string templateFormat, IPromptTemplateFactory promptTemplateFactory)
+    {
+        Console.WriteLine($"======== {templateFormat} : {prompt} ========");
+
+        KernelFunction function = kernel.CreateFunctionFromPrompt(
+            promptConfig: new PromptTemplateConfig
+            {
+                Template = prompt,
+                TemplateFormat = templateFormat,
+                Name = "MyFunction"
+            },
+            promptTemplateFactory: promptTemplateFactory);
+
+        KernelArguments arguments = new()
+        {
+            {"name",name }
+        };
+
+        FunctionResult result = await kernel.InvokeAsync(function, arguments);
+
+        Console.WriteLine(result.GetValue<string>());
+    }
+}
---
+++ quickstarts/Concepts/PromptTemplates/PromptFunctionsWithChatGPT.cs
@@ -0,0 +1,18 @@
+﻿namespace PromptTemplates;
+
+public class PromptFunctionsWithChatGPT(ITestOutputHelper output) : BaseTest(output)
+{
+    [Fact]
+    public async Task RunAsync()
+    {
+        this.WriteLine("======== Using Chat GPT model for text generation ========");
+
+        Kernel kernel = KernelHelper.AzureOpenAIChatCompletionKernelBuilder().Build();
+
+        KernelFunction kernelFunction = kernel.CreateFunctionFromPrompt("List the two planets closest to '{{$input}}', excluding moons, using bullet points.");
+
+        FunctionResult functionResult = await kernel.InvokeAsync(kernelFunction, new() { ["input"] = "Earth" });
+
+        this.WriteLine(functionResult.GetValue<string>());
+    }
+}
