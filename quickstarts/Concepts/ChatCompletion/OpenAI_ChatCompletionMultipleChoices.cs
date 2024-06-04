1dd8cbfb84c6109fa8b04ad7099660e72d333e43
ShuaiHua Du <shuaihua.du@outlook.com>
2024-06-05 02:31:31 +0800

    Add examples.

--- quickstarts/Concepts/ChatCompletion/OpenAI_ChatCompletionMultipleChoices.cs
+++ quickstarts/Concepts/ChatCompletion/OpenAI_ChatCompletionMultipleChoices.cs
@@ -41,4 +41,39 @@ public class OpenAI_ChatCompletionMultipleChoices(ITestOutputHelper output) : Ba
 
         Console.WriteLine();
     }
+
+    private async Task RunChatAsync(IChatCompletionService chatCompletionService)
+    {
+        var chatHistory = new ChatHistory("You are a librarian, expert about books");
+
+        chatHistory.AddUserMessage("Hi, I'm looking for book 3 different book suggestions about sci-fi");
+        await MessageOutputAsync(chatHistory);
+
+        var chatExecutionSettings = new OpenAIPromptExecutionSettings()
+        {
+            MaxTokens = 1024,
+            ResultsPerPrompt = 2,
+            Temperature = 1,
+            TopP = 0.5,
+            FrequencyPenalty = 0,
+        };
+
+        foreach (var chatMessageChoice in await chatCompletionService.GetChatMessageContentsAsync(chatHistory, chatExecutionSettings))
+        {
+            chatHistory.Add(chatMessageChoice!);
+            await MessageOutputAsync(chatHistory);
+        }
+
+        this.WriteLine();
+    }
+
+    private Task MessageOutputAsync(ChatHistory chatHistory)
+    {
+        var message = chatHistory.Last();
+
+        this.WriteLine($"{message.Role}: {message.Content}");
+        this.WriteLine("------------------------");
+
+        return Task.CompletedTask;
+    }
 }
